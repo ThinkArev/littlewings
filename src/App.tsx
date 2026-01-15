2a358e4a116fe7357c1412943b5efa0a53b4c416
Gagan <gagan.gupta@scoreme.in>
2026-01-15 20:15:15 +0530

    Feat : added the analytics for the webcode vitals ,

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import AboutPage from "./pages/AboutPage";
 import StaffPage from "./pages/StaffPage";
@@ -50,6 +51,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
